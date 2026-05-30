4d6a9bae5301355fa0b4c52a4c2aea53c820eb2e
Camila Benitez <camibenitez61@gmail.com>
2026-05-30 10:26:37 -0300

    feat: registrar rutas de productos en URLs principales

--- config/urls.py
+++ config/urls.py
@@ -29,6 +29,7 @@ urlpatterns = [
 
     #  Rutas de la app "usuarios"
     path("api/usuarios/", include("apps.usuarios.urls")),  # Incluye las rutas definidas en apps/usuarios/urls.py
+    path("api/productos/", include("apps.productos.urls")),  # Incluye las rutas definidas en apps/productos/urls.py
 ]
 
 # En desarrollo, sirve archivos estáticos y de medios desde sus directorios configurados
