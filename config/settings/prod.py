14d5bbbec8e7734af4546e36f04f62517e26f2af
Camila Benitez <camibenitez61@gmail.com>
2026-06-28 18:17:07 -0300

    feat: agregar directivas de seguridad HTTPS (HSTS, cookies seguras, SSL redirect) en settings de produccion

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,8 +1,18 @@
-# Importamos toda la configuración común definida en base.py
+# Importamos toda la configuracion comun definida en base.py
 from .base import *
 
-# En producción siempre debe estar en False.
+# En produccion siempre debe estar en False.
 DEBUG = False
 
-# ALLOWED_HOSTS define qué dominios o IPs pueden acceder a la aplicación.
+# ALLOWED_HOSTS define que dominios o IPs pueden acceder a la aplicacion.
 ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="").split(",")
+
+USAR_HTTPS = config("USAR_HTTPS", default=True, cast=bool)
+
+if USAR_HTTPS:
+    SECURE_SSL_REDIRECT = True
+    SESSION_COOKIE_SECURE = True
+    CSRF_COOKIE_SECURE = True
+    SECURE_HSTS_SECONDS = 60 * 60 * 24 * 30  # 30 dias
+    SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+    SECURE_HSTS_PRELOAD = True
